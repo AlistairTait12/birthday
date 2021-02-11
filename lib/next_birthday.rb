d71137f269c315b7584761173effd415f71c29e3
AlistairTait12 <alistair.tait1@gmail.com>
2021-02-11 23:20:34 +0000

    skeleton NextBirthday class

--- lib/next_birthday.rb
+++ lib/next_birthday.rb
@@ -1,3 +1,17 @@
-class BirthdayCalculator
-  
+require 'date'
+
+class NextBirthday
+  def initialize
+    # inits with a value of their birthday this year
+  end
+
+  def next
+    # calculates whether this year or next
+  end
+
+  private
+
+  def occurred?
+    # used in #next to see if this year's has been
+  end
 end
